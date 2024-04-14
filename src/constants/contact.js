51efdb560adc3ae58e861295b31f066a7e5ef6dd
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-04-14 07:45:15 +0100

    Add contact page

---
+++ src/constants/contact.js
@@ -0,0 +1,34 @@
+export const contactDetails = [
+  {
+    location: "Hamilton, Ontario",
+    addressLn1: "293 Wellington Street N",
+    addressLn2: "Suite 105",
+    addressLn3: "Hamilton ON L8L 8E7",
+    cord1: "905.521.8888",
+    cord2: "1.888.VIRK.LAW",
+  },
+  {
+    location: "Brampton, Ontario",
+    addressLn1: "2 County Ct Blvd",
+    addressLn2: "Suite 433",
+    addressLn3: "Brampton ON L6W 3X7",
+    cord1: "905.521.8888",
+    cord2: "1.855.847.5529",
+  },
+  {
+    location: "Milton, Ontario",
+    addressLn1: "500 Steeles Ave E",
+    addressLn2: "Unit 3",
+    addressLn3: "Milton ON L9T 3P7",
+    cord1: "905.521.8888",
+    cord2: "1.855.847.5529",
+  },
+  {
+    location: "Burlington, Ontario",
+    addressLn1: "4145 N Service Rd",
+    addressLn2: "Suite 205",
+    addressLn3: "Burlington ON L7L 4X6",
+    cord1: "905.521.8888",
+    cord2: "1.855.847.5529",
+  },
+];
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,12 +4,130 @@ import NewsLetter from "../components/Newsletter";
 import Typography from "../components/Typography";
 import ContactForm from "../components/ContactForm";
 import Image from "../assets/caret-red.svg";
+import { contactDetails } from "../constants/contact";
 
 const Contact = () => {
   return (
     <section className="bg-primary-dark pb-10">
       <div>
         <img src={ContactImg} alt="Contact" className="grayscale" />
+      </div>
+
+      <div className="max-w-7xl mx-auto border-t border-gray-300 py-4 mt-10">
+        {contactDetails.map((contact, id) => (
+          <div
+            className="grid grid-cols-[500px_400px_400px] border-b py-4 border-gray-300"
+            key={id}
+          >
+            <Typography
+              variant="headline-s"
+              fontWeight="regular"
+              customClassName="!text-white md:max-w-xs"
+            >
+              {contact.location}
+            </Typography>
+            <div>
+              <Typography
+                variant="title-m"
+                fontWeight="regular"
+                customClassName="md:max-w-xs mb-2"
+              >
+                Address
+              </Typography>
+              <Typography fontWeight="regular" customClassName="!text-white">
+                {contact.addressLn1}
+              </Typography>
+              <Typography fontWeight="regular" customClassName="!text-white">
+                {contact.addressLn2}
+              </Typography>
+              <Typography fontWeight="regular" customClassName="!text-white">
+                {contact.addressLn3}
+              </Typography>
+            </div>
+            <div>
+              <Typography
+                variant="title-m"
+                fontWeight="regular"
+                customClassName="md:max-w-xs mb-2"
+              >
+                Email
+              </Typography>
+              <Typography customClassName="!text-white md:max-w-xs">
+                info@virklawyers.com
+              </Typography>
+              <Typography
+                variant="title-m"
+                fontWeight="regular"
+                customClassName="md:max-w-xs my-2"
+              >
+                Fax
+              </Typography>
+              <Typography
+                fontWeight="regular"
+                customClassName="!text-white md:max-w-xs"
+              >
+                905.521.8858
+              </Typography>
+            </div>
+
+            <div className="col-end-4 col-span-2">
+              <Typography
+                variant="headline-l"
+                fontWeight="regular"
+                customClassName="!text-white md:max-w-xs"
+              >
+                {contact.cord1}
+              </Typography>
+              <Typography
+                variant="headline-l"
+                fontWeight="regular"
+                customClassName="!text-white md:max-w-xs"
+              >
+                {contact.cord2}
+              </Typography>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="max-w-5xl flex justify-between my-20 mx-auto">
+        <Typography
+          fontWeight="regular"
+          variant="headline-m"
+          customClassName="!text-white"
+        >
+          You can also find us in:
+        </Typography>
+        <div className="space-y-4">
+          <Typography
+            fontWeight="regular"
+            variant="headline-m"
+            customClassName="!text-white"
+          >
+            Mississauga, Ontario
+          </Typography>
+          <Typography
+            fontWeight="regular"
+            variant="headline-m"
+            customClassName="!text-white"
+          >
+            Niagara Falls, Ontario
+          </Typography>
+          <Typography
+            fontWeight="regular"
+            variant="headline-m"
+            customClassName="!text-white"
+          >
+            St. Catharines, Ontario
+          </Typography>
+          <Typography
+            fontWeight="regular"
+            variant="headline-m"
+            customClassName="!text-white"
+          >
+            Toronto, Ontario
+          </Typography>
+        </div>
       </div>
 
       <div className="bg-white p-20 flex justify-between mt-10 max-w-6xl mx-auto">
